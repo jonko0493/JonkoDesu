9851ea69ed636f10ebfc5763b6e0866acd288123
jonko0493 <jonko0493@protonmail.com>
2026-04-07 06:40:01 +0900

    Fixup about me page

--- src/routes/about-me.tsx
+++ src/routes/about-me.tsx
@@ -24,24 +24,26 @@ function RouteComponent() {
 
             <main className='monitor-content'>
               <section className='card'>
-                <h2>Jonko, YouTuber and ROM Hacker</h2>
-                <p>
-                  I'm Jonko — a ROM hacker, homebrew developer, and YouTuber. My primary work is with the{' '}
-                  <a href="https://haroohie.club/">Haroohie Translation Club</a>, where I serve as project
-                  lead and main ROM hacker, producing fan translations of Japanese games.
-                </p>
-                <p>
-                  You may also know me from my <a href="https://youtube.com/@jonko0493">YouTube channel</a>{' '}
-                  or my <a href="https://haroohie.club/author/jonko">blogs</a> where I explain how ROM hacking
-                  works. I also have an upcoming (secret) homebrew game I'm developing with a talented team.
-                </p>
+                <div className='card-body'>
+                  <h2>Jonko, YouTuber and ROM Hacker</h2>
+                  <p>
+                    I'm Jonko — a ROM hacker, homebrew developer, and YouTuber. My primary work is with the{' '}
+                    <a href="https://haroohie.club/">Haroohie Translation Club</a>, where I serve as project
+                    lead and main ROM hacker, producing fan translations of Japanese games.
+                  </p>
+                  <p>
+                    You may also know me from my <a href="https://youtube.com/@jonko0493">YouTube channel</a>{' '}
+                    or my <a href="https://haroohie.club/author/jonko">blogs</a> where I explain how ROM hacking
+                    works. I also have an upcoming (secret) homebrew game I'm developing with a talented team.
+                  </p>
+                </div>
 
-                <div className="socials">
+                <footer className="socials">
                   <a href="https://twitter.com/jonko0493" aria-label="Twitter"><FontAwesomeIcon icon={faTwitter} /></a>
                   <a href="https://youtube.com/@jonko0493" aria-label="YouTube"><FontAwesomeIcon icon={faYoutube} /></a>
                   <a href="https://bsky.app/profile/jonkode.su" aria-label="Bluesky"><FontAwesomeIcon icon={faBluesky} /></a>
                   <a href="https://github.com/jonko0493" aria-label="GitHub"><FontAwesomeIcon icon={faGithub} /></a>
-                </div>
+                </footer>
               </section>
             </main>
           </div>
